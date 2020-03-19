63674c3021dcab3bcd8163323f412be19d4e40fb
Alexey Kutepov <alkutepov@gmail.com>
2020-03-19 20:37:29 +0300

    Initial commit

---
+++ core/src/ru/geekbrains/StarGame.java
@@ -0,0 +1,39 @@
+package ru.geekbrains;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class StarGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	TextureRegion region;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		region = new TextureRegion(img, 10, 10, 150, 150);
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(0.5f, 0.7f, 0.8f, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.setColor(0.4f, 0.6f, 0.3f, 1f);
+		batch.draw(img, 0, 0);
+		batch.setColor(0.9f, 0.3f, 0.7f, 0.5f);
+		batch.draw(region, 200, 200);
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
