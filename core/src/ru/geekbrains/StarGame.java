766e120dd78035c3ae33cecbd6a08f4c70dfce8b
Alexey Kutepov <alkutepov@gmail.com>
2020-03-19 20:44:25 +0300

    Lesson 1

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -24,6 +24,7 @@ public class StarGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0.5f, 0.7f, 0.8f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+		batch.setColor(0.1f, 0.3f, 0.6f, 0.5f);
 		batch.draw(img, 0, 0);
 		batch.draw(region, 200, 200);
 		batch.end();
