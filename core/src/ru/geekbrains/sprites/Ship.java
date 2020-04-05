910b47326da547a5bfb0f99df524bc49e41e536f
palandarus <klarkken@yandex.ru>
2020-04-05 16:14:44 +0300

    reformated ship movement

--- core/src/ru/geekbrains/sprites/Ship.java
+++ core/src/ru/geekbrains/sprites/Ship.java
@@ -22,37 +22,36 @@ public class Ship extends Sprite {
     public Ship(TextureAtlas atlas) throws GameException {
         super(atlas.findRegion("main_ship"));
         setScale(0.1f);
-        pos.set(STARTX,STARTY);
-        touchCalcTMP=new Vector2();
-        touch=new Vector2(pos);
-        vShip=new Vector2(0f,-0.45f);
-        regions[0]=new TextureRegion(regions[0],0,0,regions[0].getRegionWidth()/2,regions[0].getRegionHeight());
+        pos.set(STARTX, STARTY);
+        touchCalcTMP = new Vector2();
+        touch = new Vector2(pos);
+        vShip = new Vector2(0.08f, 0f);
+        regions[0] = new TextureRegion(regions[0], 0, 0, regions[0].getRegionWidth() / 2, regions[0].getRegionHeight());
 
     }
 
     @Override
     public void resize(Rect worldBounds) {
         setHeightProportion(0.5f);
-        this.worldBounds=worldBounds;
+        this.worldBounds = worldBounds;
 
     }
 
     @Override
     public void update(float delta) {
         touchCalcTMP.set(touch);
-        if(touchCalcTMP.sub(pos).len()>VLEN) {
-            pos.add(vShip);
-        }
-        else{
+        if (Math.abs(touchCalcTMP.sub(pos.x, 0).x) > VLEN) {
+            pos.add(vShip.x, 0);
+        } else {
             vShip.setZero();
-            pos.set(touch);
+            pos.set(touch.x, STARTY);
         }
     }
 
     public void touchDown(Vector2 touch) {
-        this.touch=touch;
+        this.touch = touch;
         touchCalcTMP.set(touch);
-        vShip.set(touchCalcTMP.sub(pos)).setLength(VLEN);
+        vShip.set(touchCalcTMP.x, 0).setLength(VLEN);
     }
 
     public void touchUp(Vector2 touch) {
