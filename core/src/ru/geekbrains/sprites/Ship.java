c5452beaef20b4e93f183034689a95a0e205e861
palandarus <klarkken@yandex.ru>
2020-04-05 19:30:53 +0300

    переработана логика движения добавлена обработка движения по нажатию клавиш на клавиатуре, а именно стрелок влево/вправо, A, D, Ф, В

--- core/src/ru/geekbrains/sprites/Ship.java
+++ core/src/ru/geekbrains/sprites/Ship.java
@@ -1,6 +1,5 @@
 package ru.geekbrains.sprites;
 
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
@@ -8,7 +7,6 @@ import com.badlogic.gdx.math.Vector2;
 import ru.geekbrains.base.Sprite;
 import ru.geekbrains.exception.GameException;
 import ru.geekbrains.math.Rect;
-import ru.geekbrains.math.Rnd;
 
 public class Ship extends Sprite {
     private Vector2 touchCalcTMP;
@@ -17,7 +15,9 @@ public class Ship extends Sprite {
     private float VLEN = 0.005f;
     private float STARTX = 0f;
     private float STARTY = -0.45f;
+    private float osXSpeed = 0.001f;
     private Rect worldBounds;
+
 
     public Ship(TextureAtlas atlas) throws GameException {
         super(atlas.findRegion("main_ship"));
@@ -25,7 +25,7 @@ public class Ship extends Sprite {
         pos.set(STARTX, STARTY);
         touchCalcTMP = new Vector2();
         touch = new Vector2(pos);
-        vShip = new Vector2(0.08f, 0f);
+        vShip = new Vector2(osXSpeed, 0f);
         regions[0] = new TextureRegion(regions[0], 0, 0, regions[0].getRegionWidth() / 2, regions[0].getRegionHeight());
 
     }
@@ -39,26 +39,68 @@ public class Ship extends Sprite {
 
     @Override
     public void update(float delta) {
+        touchCalcTMP.set(pos);
+        touchCalcTMP.add(vShip);
+        if (Math.abs(touchCalcTMP.x) > (worldBounds.getHalfWidth() - this.getHalfWidth() / 10))
+            vShip.setZero();
+
         touchCalcTMP.set(touch);
-        if (Math.abs(touchCalcTMP.sub(pos.x, 0).x) > VLEN) {
-            pos.add(vShip.x, 0);
+        if (touchCalcTMP.sub(pos).len() > VLEN) {
+            pos.add(vShip);
         } else {
             vShip.setZero();
-            pos.set(touch.x, STARTY);
+            pos.set(touch);
         }
+
     }
 
     public void touchDown(Vector2 touch) {
-        this.touch = touch;
-        touchCalcTMP.set(touch);
-        vShip.set(touchCalcTMP.x, 0).setLength(VLEN);
+        move(touch);
     }
 
     public void touchUp(Vector2 touch) {
-        this.touch.set(touch);
+        move(touch);
     }
 
     public void touchDragged(Vector2 touch) {
-        this.touch.set(touch);
+        move(touch);
+    }
+
+    public void keyDown(int keycode) {
+        switch (keycode) {
+            case 21:
+                moveFromKey(-worldBounds.getHalfWidth());
+                break;
+            case 29:
+                moveFromKey(-worldBounds.getHalfWidth());
+                break;
+
+            case 22:
+                moveFromKey(worldBounds.getHalfWidth());
+                break;
+
+            case 32:
+                moveFromKey(worldBounds.getHalfWidth());
+                break;
+
+        }
+    }
+
+
+    @Override
+    public void keyUp(int keycode) {
+        touch.set(pos.x, pos.y);
+        vShip.set(0, 0);
+    }
+
+    private void move(Vector2 touch) {
+        this.touch.set(touch.x, STARTY);
+        touchCalcTMP.set(this.touch);
+        vShip.set(touchCalcTMP.sub(pos)).setLength(VLEN);
+    }
+
+    private void moveFromKey(float v) {
+        touch.set(v, 0f);
+        vShip.set(touch.x, 0).setLength(VLEN);
     }
 }
